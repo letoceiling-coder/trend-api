8ac8199994d1c301bf82704c6f252f8d891291a8
letoceiling-coder <dsc-23@yandex.ru>
2026-02-11 07:29:17 +0300

    deploy: 2026-02-11 04:29

--- backend/database/migrations/2026_02_10_192757_create_ta_directories_table.php
+++ backend/database/migrations/2026_02_10_192757_create_ta_directories_table.php
@@ -11,6 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('ta_directories')) {
+            return;
+        }
         Schema::create('ta_directories', function (Blueprint $table) {
             $table->id();
             $table->string('type', 100); // 'rooms', 'deadlines', 'regions', etc.
